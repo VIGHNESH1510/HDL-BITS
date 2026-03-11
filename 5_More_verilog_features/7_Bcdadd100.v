60b2352cfa6af6aaa38a8acc4b9b4dbcbe4b4d20
Vighnesh E S <Vighnesh1510@gmail.com>
2026-03-11 22:40:52 +0530

    Update 7_Bcdadd100.v

--- 5_More_verilog_features/7_Bcdadd100.v
+++ 5_More_verilog_features/7_Bcdadd100.v
@@ -3,15 +3,17 @@ module top_module(
     input cin,
     output cout,
     output [399:0] sum );
-    wire [400:0] carry;
-    assign carry[0] = cin;
-    assign cout =carry[400];
+    wire [400:0] carry;         //wire [100:0] carry;
+    assign carry[0] = cin; 
+    assign cout =carry[400];    //assign cout =carry[100];
     genvar i;
     generate 
         for(i=0; i<400 ;i=i+4)
             begin: bcd
-                bcd_fadd ins(a[i+3:i],b[i+3:i],carry[i],carry[i+4],sum[i+3:i]);
+                bcd_fadd ins(a[i+3:i],b[i+3:i],carry[i],carry[i+4],sum[i+3:i]);  //bcd_fadd ins(a[i+3:i],b[i+3:i],carry[i/4],carry[(i/4)+1],sum[i+3:i]);
             end
     endgenerate
 
 endmodule
+
+
