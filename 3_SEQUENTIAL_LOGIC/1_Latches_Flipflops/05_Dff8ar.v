8424a6951b6b8e3fa1aa8a6cb355d5b6aedcfbbb
Vighnesh E S <Vighnesh1510@gmail.com>
2026-03-17 22:15:44 +0530

    Create 05_Dff8ar.v

---
+++ 3_SEQUENTIAL_LOGIC/1_Latches_Flipflops/05_Dff8ar.v
@@ -0,0 +1,12 @@
+module top_module (
+    input clk,
+    input areset,   // active high asynchronous reset
+    input [7:0] d,
+    output [7:0] q
+);
+    always @(posedge clk or posedge areset)
+        begin
+            q <= areset? 8'd0: d;
+        end
+
+endmodule
