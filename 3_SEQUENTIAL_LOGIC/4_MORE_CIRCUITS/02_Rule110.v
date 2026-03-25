20c6b14cb7ee8d3ece864b6e9539ceb3f30a04ab
Vighnesh E S <Vighnesh1510@gmail.com>
2026-03-25 18:57:02 +0530

    Create 02_Rule110.v

---
+++ 3_SEQUENTIAL_LOGIC/4_MORE_CIRCUITS/02_Rule110.v
@@ -0,0 +1,18 @@
+module top_module(
+    input clk,
+    input load,
+    input [511:0] data,
+    output reg [511:0] q
+);
+
+    wire [511:0] left  = {1'b0, q[511:1]};
+    wire [511:0] right = {q[510:0], 1'b0};
+
+    always @(posedge clk) begin
+        if (load)
+            q <= data;
+        else
+            q <= (q ^ right) | (~left & right);
+    end
+
+endmodule
